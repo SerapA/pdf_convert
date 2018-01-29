5d056fe55cbd6c874240cf172741232e0276dca6
rivkawaxman <rivka@bitbean.com>
2018-01-29 10:12:29 -0500

    first commit

---
+++ PdfConvert/Controllers/PdfController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PdfConvert.Controllers
+{
+    [Route("api/[controller]")]
+    public class PdfController : Controller
+    {
+        private IConverter _converter;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public PdfController(IConverter converter, IHostingEnvironment hostingEnvironment)
+        {
+            _converter = converter;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.CompletedTask;
+            string html = "<!DOCTYPE html>" +
+                            "<html lang = \"en\">" +
+                                 "<head>" +
+                                     "<meta charset = \"UTF-8\">" +
+                                     " <title> Check </title>" +
+                                      "<style>" +
+                                                "@font-face {" +
+                                                   "font-family: 'MICR';" +
+                                                    "src: url(\"micrenc.woff\") format(\"woff\");" +
+                                                    "font-weight: normal; " +
+                                                    "font-style: normal; " +
+                                                "}" +
+                                    "</style> " +
+                                "</head>" +
+                                "<body>" +
+                                    "<div style = \"text-align: center; margin-top:16px;\">" +
+                                        "<p style=\"font-family: 'MICR'; font-size: 30px;\"> 235 384756  13425346 9877</p>" +
+                                    "</div>" +
+                                "</body>" +
+                            "</html>";
+
+            var pdf = new HtmlToPdfConverterService(_converter).Convert(html);
+            var path = Path.Combine(_hostingEnvironment.WebRootPath, "micr.pdf");
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(pdf, 0, pdf.Length);
+            }
+            return Ok();
+        }
+
+    }
+}
---
+++ PdfConvert/HtmlToPdfConverterService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+
+namespace PdfConvert
+{
+    public class HtmlToPdfConverterService
+    {
+        private IConverter _converter;
+
+        public HtmlToPdfConverterService(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        public byte[] Convert(string html){
+            HtmlToPdfDocument doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.Letter,
+                    Margins = new MarginSettings() { Top = 10 , Bottom=0, Left=10, Right=10},
+                    
+                },
+                Objects = {
+                        new ObjectSettings() {
+                                 HtmlContent = html
+                            }
+                        }
+            };
+
+            return _converter.Convert(doc);
+        }
+
+      
+    }
+}
